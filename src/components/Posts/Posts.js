d21cdb6f6cd6e7b16f2cad8587a9670148a63354
Az-s <azim.sa@yahoo.com>
2021-08-28 01:17:34 +0600

    some fixes in post.js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -11,9 +11,11 @@ const Posts = () => {
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get('https://az-sa-bd3f9-default-rtdb.firebaseio.com/posts.json');
-            setAllPosts(response.data);
-            console.log(response.data)
-            console.log(allPosts)
+            const allPosts = Object.keys(response.data).map(id => ({
+                ...response.data[id],
+                id,
+            }))
+            setAllPosts(allPosts);
         };
 
         fetchData().catch(console.error);
@@ -22,10 +24,10 @@ const Posts = () => {
     return (
         <>
             {allPosts.map(post => (
-                <Card style={{ marginTop: '5rem' }} className='mx-5' key={id}>
+                <Card style={{ marginTop: '5rem' }} className='mx-5' key={post.id} >
                     <Card.Header>Created on: {post.date}</Card.Header>
                     <Card.Body>
-                        <Card.Title>{post.title}</Card.Title>
+                        <Card.Title>{post.post.title}</Card.Title>
                         <NavLink to='/post'><Button variant="outline-secondary" className='mt-2 btn-p'>Read more &#xbb;</Button></NavLink>
                     </Card.Body>
                 </Card>
